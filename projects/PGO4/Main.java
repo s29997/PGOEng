24e396a762d78726dccd3f52dac1e5f384e4e942
mikitaliashchuk <s29997@pjwstk.edu,pl>
2024-04-16 12:31:22 +0200

    Cleanup

--- projects/PGO4/Main.java
+++ projects/PGO4/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        new StudentGroup(scanner.next());
-        new Student(scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.next());
+        StudentGroup group1 = new StudentGroup(scanner.next());
+        Student student1 = new Student(scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.next());
+        group1.addStudents(student1.indexNumber);
     }
 }
