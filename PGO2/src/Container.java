8dcc4058d5f8b7ca50c60f914c5e575f4ca3dfd0
mikitaliashchuk <s29997@pjwstk.edu,pl>
2024-04-16 10:55:31 +0200

    REPO UPDATE

--- PGO2/src/Container.java
+++
@@ -1,6 +0,0 @@
-public class Container {
-    public int Serial;
-    public int type;
-    public int rate;
-    public int currentWeight;
-}
--- PGO2/src/Employee.java
+++
@@ -1,7 +0,0 @@
-public class Employee {
-    public String Fname;
-    public String Lname;
-    public int Phone;
-    public int Pesel;
-    public String Class;
-}
