77d77f0dea35f3672e328c27e6e80cdccb687ad4
mikitaliashchuk <s29997@pjwstk.edu,pl>
2024-04-16 12:23:54 +0200

    Cleanup

--- projects/PGO4/Main.java
+++ projects/PGO4/Main.java
@@ -2,12 +2,8 @@ package PGO4;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner FName = new Scanner(System.in);
-        Scanner LName = new Scanner(System.in);
-        Scanner indexNumber = new Scanner(System.in);
-        Scanner email = new Scanner(System.in);
-        Scanner address = new Scanner(System.in);
-
-
+        Scanner scanner = new Scanner(System.in);
+        new StudentGroup(scanner.next());
+        new Student(scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.next());
     }
 }
--- projects/PGO4/StudentGroup.java
+++ projects/PGO4/StudentGroup.java
@@ -3,13 +3,14 @@ import java.util.ArrayList;
 import java.util.List;
 public class StudentGroup {
     public String groupName;
-    public List<String> students;
-    private int allowedGroupSize = 15;
+    public  List<String> students;
+
     public StudentGroup(String groupName) {
         this.groupName=groupName;
         this.students=new ArrayList<>();
     }
     public void addStudents(String student) {
+        int allowedGroupSize = 15;
         if (students.size() >= allowedGroupSize) {
             throw new IllegalStateException("There is already maximum amount of students in the group. Cannot add more students(");
         }
