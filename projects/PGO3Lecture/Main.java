772d1bd9fe04df4ba4b11089b06a5eb18c220ecb
mikitaliashchuk <s29997@pjwstk.edu,pl>
2024-04-16 12:02:25 +0200

    Cleanup

--- projects/PGO3Lecture/Main.java
+++ projects/PGO3Lecture/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Employee s1 = new Employee();
         s1.Pesel = 123;
+        s1.Fname = "Name";
+
     }
 }
--- projects/idkSomeLostFiles/Container.java
+++
@@ -1,7 +0,0 @@
-package idkSomeLostFiles;
-public class Container {
-    public int Serial;
-    public int type;
-    public int rate;
-    public int currentWeight;
-}
